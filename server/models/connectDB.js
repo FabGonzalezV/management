9ada7ead0997a5d0baeab933f36dda5beb73f970
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-10-05 14:28:48 -0600

    connectDB.js updated

--- server/models/connectDB.js
+++ server/models/connectDB.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
- const uri = process.env.mongoUriAtlas;
-
+const uri = process.env.mongoUriAtlas;
 const connectDB = async () => {
   try {
     await mongoose.connect(uri, {
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 import app from "./app";
 import watchDog from "./helpers/port-watchdog";
 
+import connectDD from "./models/connectDB";
+connectDD();
 const port = import.meta.env.PORT;
 const server = app.listen(port, (error) => {
-    if (!error) {
-        console.info(`Server is running on port ${import.meta.env.PORT}`);
-    } else {
-        console.error("Error occured while starting the server");
-
-    }
+  if (!error) {
+    console.info(`Server is running on port ${import.meta.env.PORT}`);
+  } else {
+    console.error("Error occured while starting the server");
+  }
 });
 
 watchDog(server, port);
